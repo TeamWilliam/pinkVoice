a5c582ede2fc4dcc8a799e75d5c89192e4c4bb64
Jihyoung <59171154+HelloJihyoung@users.noreply.github.com>
2021-02-17 01:20:29 +0900

    미완.. 내일 더 수정예정

--- android/app/src/main/java/com/example/myapplication3/SubwayTimeActivity.java
+++ android/app/src/main/java/com/example/myapplication3/SubwayTimeActivity.java
@@ -1,14 +1,86 @@
 package com.example.myapplication3;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SubwayTimeActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subway_time);
+
+        Button btn = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getApi();
+            }
+        });
+
+    
+    }
+
+    private void getApi() {
+        new AsyncTask<Void, Void, String>() {
+            ProgressDialog progress;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progress = new ProgressDialog(SubwayTimeActivity.this);
+                progress.setTitle("다운로드");
+                progress.setMessage("download");
+                progress.setProgressStyle((ProgressDialog.STYLE_SPINNER));
+                progress.setCancelable(false);
+                progress.show();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                StringBuffer sb = new  StringBuffer();
+                try {
+                    JSONObject json = new JSONObject(s);
+
+                    JSONArray rows = json.getJSONArray("realtimeArrivalList");
+
+                    int length = rows.length();
+                    for(int i=0; i < length; i ++){
+                        JSONObject result = (JSONObject) rows.get(i);
+                        String trainName = result.getString("trainLineNm");
+                        sb.append(trainName + "\n");
+
+                    }
+
+                }catch (Exception e ){}
+
+                textView.setText(sb.toString());
+                progress.dismiss();
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                String result = "";
+                try {
+                    //서울시 오픈 API 제공(샘플 주소 json으로 작업)
+                    result = Remote.getData("http://swopenapi.seoul.go.kr/api/subway/534c5457527373753333556c6c476f/xml/realtimeStationArrival/0/5/%EC%B6%A9%EB%AC%B4%EB%A1%9C");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                return result;
+            }
+        }.execute();
+
     }
 }
