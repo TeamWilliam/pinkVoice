23e0839dbd24d63da32a6e01f715d7a0b5881fa7
zio <jiho8345@naver.com>
2021-02-17 12:09:41 +0900

    하차하기 완료

--- pinkVoice-main/android/app/src/main/java/com/example/myapplication3/UseSeat.java
+++ pinkVoice-main/android/app/src/main/java/com/example/myapplication3/UseSeat.java
@@ -3,10 +3,12 @@ package com.example.myapplication3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class UseSeat extends AppCompatActivity {
 
@@ -25,6 +27,29 @@ public class UseSeat extends AppCompatActivity {
                 TextView takeoff_text = (TextView)takeoff_dialog.findViewById(R.id.noti);
                 takeoff_text.setText("\n이용을 완료하시겠습니까?\n");
                 takeoff_dialog.show();
+                //Cancel 버튼 핸들러
+                Button noBtn = takeoff_dialog.findViewById(R.id.custom_cancel);
+                noBtn.setOnClickListener(new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(UseSeat.this,"아니오를 선택했습니다.",Toast.LENGTH_LONG).show();
+                        takeoff_dialog.dismiss();
+                    }
+                });
+                //OK 버튼 핸들러
+                Button yesBtn = takeoff_dialog.findViewById(R.id.custom_confirm);
+                yesBtn.setOnClickListener(new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(UseSeat.this,"이용을 종료합니다.",Toast.LENGTH_LONG).show();
+                        //이용 종료 디비에서 좌석 상태 빈좌석으로 바꾸기
+
+
+                        //메인화면으로 이동
+                        Intent takeoffIntent = new Intent(getBaseContext(), MainActivity.class);
+                        startActivity(takeoffIntent);
+                    }
+                });
 
             }
         });
