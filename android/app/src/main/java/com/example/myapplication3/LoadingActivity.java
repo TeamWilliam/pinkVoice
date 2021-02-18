a632b14afc3a1fdd5d342a20f367889d10502e49
ssuh0o0 <ssuh0o0@daum.net>
2021-02-18 16:42:53 +0900

    main to login

--- android/app/src/main/java/com/example/myapplication3/LoadingActivity.java
+++ android/app/src/main/java/com/example/myapplication3/LoadingActivity.java
@@ -20,7 +20,7 @@ public class LoadingActivity extends Activity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
