c491005cd07dea9684dd39def6be072eb3f7fe1a
Eun Young <eyeyl0811@naver.com>
2021-02-18 15:36:04 +0900

    fragment 오류 해결

--- android/app/src/main/java/com/example/myapplication3/HomeFragment.java
+++ android/app/src/main/java/com/example/myapplication3/HomeFragment.java
@@ -19,16 +19,17 @@ import com.google.zxing.integration.android.IntentIntegrator;
 public class HomeFragment extends Fragment {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
 
         ScrollView scrollView;
         ImageView imageView;
         BitmapDrawable bitmap;
 
         //지하철 노선도 사진 HorizontalScrollView
-        scrollView = (ScrollView)findViewById(R.id.scrollView);
-        imageView = (ImageView)findViewById(R.id.imageView);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        scrollView = (ScrollView)v.findViewById(R.id.scrollVieww);
+        imageView = (ImageView)v.findViewById(R.id.imageView);
         scrollView.setHorizontalScrollBarEnabled(true);
 
         Resources res = getResources();
@@ -40,7 +41,7 @@ public class HomeFragment extends Fragment {
         imageView.getLayoutParams().width = bitmapWidth;
         imageView.getLayoutParams().height = bitmapHeight;
 
-        Button button =findViewById(R.id.Chungmuro);
+        Button button = (Button)v.findViewById(R.id.Chungmuro);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,17 +50,16 @@ public class HomeFragment extends Fragment {
             }
         });
 
-
-
+        return v;
     }
 
 
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_home, container, false);
+//    }
 }
